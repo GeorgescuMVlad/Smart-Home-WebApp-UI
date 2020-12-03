27ccf25d4ae0ba3253e0bbb7d2b82e57e047240c
Vlad Georgescu <vlad.georgescu@wirtek.com>
2020-12-03 10:22:48 +0200

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import HomePage from "./components/home-page/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePageMenu from './components/home-page/home-page-menu/HomePageMenu';
 
 function App() {
   return (
-    <div className="App">
-        <HomePage/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/home">
+          <HomePageMenu />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/home-page/home-page-menu/HomePageMenu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../../Header";
+
+export default function HomePageMenu() {
+
+    return (
+        <div>
+            <Header headerText="Home" />
+        </div>
+    );
+}
